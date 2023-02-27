b553d960f4c1404cbca7b4330e3591e4f58ef5f9
https://ci-1.ppluto.dev/ppluto-dev/ppluto-main/5309 <>
2023-02-27 13:04:15 +0000

    .

--- auto_branch_pushes/yansfil/add-ci-garbage-collect/_scripts/get_active_branches_on_pull_request.py
+++ auto_branch_pushes/yansfil/add-ci-garbage-collect/_scripts/get_active_branches_on_pull_request.py
@@ -2,7 +2,8 @@
 
 # this has a permission that reads pull request (expired in 02.24.2024)
 # TODO: it needs to be managed in env or other secret tools
-ACCESS_TOKEN="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"
+ACCESS_TOKEN = "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"
+
 
 def get_active_branches_on_pull_request():
     import requests
@@ -11,17 +12,19 @@ def get_active_branches_on_pull_request():
     username = "ppluto-dev"
     repository = "ppluto-main"
 
-    # Replace with the number of the pull request that you want to get the branches for
-    pull_request_number = 123
-
     # Set up the API endpoint URL
-    url = f"https://api.github.com/repos/{username}/{repository}/pulls?state=open"
+    url = f"https://api.github.com/repos/{username}/{repository}/pulls"
 
     headers = {
         "Accept": "application/vnd.github.v3+json",
         "Authorization": f"Bearer {ACCESS_TOKEN}",
     }
     response = requests.get(url, headers=headers)
-    branches = [pull_request["head"]["ref"] for pull_request in response.json() if pull_request["draft"] == False]
+
+    branches = [
+        pull_request["head"]["ref"]
+        for pull_request in response.json()
+        if pull_request["draft"] == False
+    ]
 
     return branches
